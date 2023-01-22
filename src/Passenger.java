ab2716264c40d63c09af5475aa0d61fabc826400
DKRajasekera <dkrajasekere@gmail.com>
2023-01-22 12:45:14 +0530

    Edite file

--- src/Passenger.java
+++ src/Passenger.java
@@ -3,21 +3,25 @@ public class Passenger {
     private String UserSurName;
     private int UserExpenses;
 
-public Passenger(String UserFirstName, String UserLastName, int UserExpenses){
-    this.UserFirstName = UserFirstName;
-    this.UserSurName = UserLastName;
-    this.UserExpenses = UserExpenses;
-}
-public String getName(){
-    return UserFirstName + " " + UserSurName;
-}
-public String getUserFirstName(){
-    return UserFirstName;
-}
-public String getUserSurName(){
-    return UserSurName;
-}
-public int getUserExpenses(){
-    return UserExpenses;
-}
+    public Passenger(String UserFirstName, String UserLastName, int UserExpenses) {
+        this.UserFirstName = UserFirstName;
+        this.UserSurName = UserLastName;
+        this.UserExpenses = UserExpenses;
+    }
+
+    public String getName() {
+        return UserFirstName + " " + UserSurName;
+    }
+
+    public String getUserFirstName() {
+        return UserFirstName;
+    }
+
+    public String getUserSurName() {
+        return UserSurName;
+    }
+
+    public int getUserExpenses() {
+        return UserExpenses;
+    }
 }
